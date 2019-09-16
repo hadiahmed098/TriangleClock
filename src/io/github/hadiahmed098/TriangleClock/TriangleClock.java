ab305f22cde7c0da3d89a68aa183773e84198e06
Hadi Ahmed <hadiahmed098@gmail.com>
2019-09-16 01:50:25 -0400

    Created package pt 3 because I can't use Git

---
+++ src/io/github/hadiahmed098/TriangleClock/TriangleClock.java
@@ -0,0 +1,40 @@
+package io.github.hadiahmed098.TriangleClock;
+import javax.swing.*;
+import java.awt.*;
+
+public class TriangleClock extends JFrame {
+    public static void main(String[] args) {
+        new TriangleClock("Triangle Clock");
+    }
+
+    public TriangleClock(String title) {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle(title);
+        setMinimumSize(new Dimension(500, 500));
+
+        //Sets up the LayeredPane to display the clock body and the clock face.
+        JLayeredPane bodyWrapper = getLayeredPane();
+        bodyWrapper.setBounds(0,0,getWidth(),getHeight());
+
+        ClockBody clockBody = new ClockBody(new Rectangle(0,0,getWidth(),getHeight()));
+        clockBody.setOpaque(false);
+        bodyWrapper.add(clockBody,JLayeredPane.DEFAULT_LAYER);
+
+        TriangleClockMechanism clockFace = new TriangleClockMechanism(new Rectangle(0,0,getWidth(),getHeight()));
+        clockFace.setOpaque(false);
+        bodyWrapper.add(clockFace,JLayeredPane.MODAL_LAYER);
+
+        setResizable(false);
+        setVisible(true);
+
+        //Repaints the frame every 100 ms to force update the graphics on the frame.
+        //Uses a lambda to avoid creating an anonymous ActionListener every run.
+        Timer t = new Timer(100, e ->
+                repaint());
+
+        t.setRepeats(true);
+        t.setCoalesce(false);
+        t.start();
+
+    }
+}
---
+++ src/io/github/hadiahmed098/TriangleClock/TriangleClockMechanism.java
@@ -0,0 +1,56 @@
+package io.github.hadiahmed098.TriangleClock;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Path2D;
+import java.awt.geom.Point2D;
+import java.util.Calendar;
+
+public class TriangleClockMechanism extends JPanel {
+
+    public TriangleClockMechanism (Rectangle bounds) {
+        setBounds(bounds);
+    }
+
+    @Override
+    protected void paintComponent(Graphics graphics) {
+        Graphics2D g2 = (Graphics2D) graphics;
+
+        Calendar current = Calendar.getInstance();
+         Point2D hour = ClockBody.getPointOf(-(current.get(Calendar.HOUR)*30 -90),150,  Math.round(getWidth() /2), Math.round(getHeight() /2));
+         Point2D minute = ClockBody.getPointOf(-(current.get(Calendar.MINUTE)*6 -90),150,  Math.round(getWidth() /2), Math.round(getHeight() /2));
+         Point2D second = ClockBody.getPointOf(-(current.get(Calendar.SECOND)*6 -90),150,  Math.round(getWidth() /2), Math.round(getHeight() /2));
+
+        g2.setStroke(new BasicStroke(10));
+
+        //Create the triangle
+        Path2D triangle = new Path2D.Double();
+        triangle.moveTo(hour.getX(),hour.getY());
+        triangle.lineTo(minute.getX(),minute.getY());
+        triangle.lineTo(second.getX(),second.getY());
+        triangle.closePath();
+
+        g2.setColor(Color.LIGHT_GRAY);
+        g2.fill(triangle);
+
+        g2.setStroke(new BasicStroke(4));
+        g2.setColor(Color.DARK_GRAY);
+        g2.draw(new Line2D.Double(hour,minute));
+        g2.draw(new Line2D.Double(hour,second));
+        g2.draw(new Line2D.Double(second,minute));
+
+        g2.setColor(Color.RED);
+        g2.fill(new Ellipse2D.Double(hour.getX()-5,hour.getY()-5,10,10));
+
+        g2.setColor(new Color(0,212,0));
+        g2.fill(new Ellipse2D.Double(minute.getX()-5,minute.getY()-5,10,10));
+
+        g2.setColor(Color.BLUE);
+        g2.fill(new Ellipse2D.Double(second.getX()-5,second.getY()-5,10,10));
+
+        //Center Point Drawing
+        g2.setColor(Color.BLACK);
+        g2.fillOval(getWidth()/2 - 5, getHeight()/2 - 5,10,10);
+    }
+}
